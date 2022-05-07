163c0074d00dd24499dc234be6767da07cf80469
Wanderson SM <88949689+WSilvaM@users.noreply.github.com>
2022-05-06 21:38:28 -0300

    Inicio controle de usuário
    Foram implementadas as classes de usuárioBLL

---
+++ Base/BLL/UsuarioBLL.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Model;
+
+namespace BLL
+{
+    public class UsuarioBLL
+    {
+       public void inserir()
+        {
+
+        }
+
+        public void Excluir()
+        {
+
+        }
+        public void Alterar()
+        {
+
+        }
+        public List<Usuario> Buscar ()
+        {
+            return new List<Usuario>();
+        }
+
+    }
+}
---
+++ Base/Model/Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Usuario
+    {
+        private string nomeUsuario;
+
+
+        public string NomeUsuario
+        {
+            get { return nomeUsuario; }
+            set { nomeUsuario = value; }
+        }
+        private bool ativo;
+        public bool Ativo
+
+
+        {
+            get { return ativo; }
+            set { ativo = value; }
+        }
+        private int id;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string senha;
+
+        public string Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+
+
+
+
+
+    }
+}
