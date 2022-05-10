15b68dc896e7c44fe8eafb039b1797365d803b28
Wanderson SM <88949689+WSilvaM@users.noreply.github.com>
2022-05-09 21:53:56 -0300

    DAL
    Criação de novas classes

---
+++ Base/DAL/ParametroSQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ParametroSQL
+    {
+        private string parametro;
+        private string valor;
+
+        public string Parametro 
+        {  
+            get { return Parametro; } 
+            set { parametro = value; }
+      
+        }
+
+        public string Valor  // propfull = atalho 
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+     
+
+    }
+}
