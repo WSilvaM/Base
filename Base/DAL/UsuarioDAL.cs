e281a0e0bbdee0da544b0c3b052cc07ee0f1b5a6
Wanderson SM <88949689+WSilvaM@users.noreply.github.com>
2022-05-10 21:50:22 -0300

    Commit
    UsuarioDAL

---
+++ Base/DAL/UsuarioDAL.cs
@@ -0,0 +1,73 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public Usuario Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_InserirUsuario";
+
+                SqlParameter pativo = new SqlParameter("@Ativo", SqlDbType.Bit)
+                {
+                    Value = _usuario.Ativo
+                };
+
+
+                cmd.Parameters.Add(pativo);
+
+                cmd.Parameters.Add(new SqlParameter("@NomeUsuario", SqlDbType.VarChar) { Value = _usuario.NomeUsuario });
+
+
+                SqlParameter psenha = new SqlParameter("@Senha", SqlDbType.VarChar)
+                {
+                    Value = _usuario.Senha
+                };
+
+                cmd.Parameters.Add(psenha);
+
+                SqlParameter pid = new SqlParameter("@Id", SqlDbType.Int)
+                {
+                    Value = _usuario.Id
+
+                };
+
+                cmd.Parameters.Add(pid);
+
+                cn.Open();
+                _usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return _usuario;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
