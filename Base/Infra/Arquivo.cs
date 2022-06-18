923a0f8df8db8c47285542c4aff84b9625c771ef
Wanderson SM <88949689+WSilvaM@users.noreply.github.com>
2022-06-17 21:29:32 -0300

    infra

---
+++ Base/Infra/Arquivo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    public class Arquivo
+    {
+        public void GravarTextNoFinalDoArquivo(string _texto, string _arquivo)
+        {
+            FileStream fileStream = new FileStream(_arquivo, FileMode.Append);
+            StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.UTF8);
+
+            streamWriter.WriteLine(_texto);
+            streamWriter.Flush();
+            streamWriter.Close();
+            fileStream.Close();
+        }
+    }
+}
